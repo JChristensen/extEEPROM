45d42a1a003d02621e2c342a8c0d6485e3faf6a5
JChristensen <j.christensen@sbcglobal.net>
2014-07-08 16:09:05 -0400

    Support for EEPROM sizes 2k bit to 2048k bit.

--- extEEPROM.cpp
+++ extEEPROM.cpp
@@ -1,87 +1,129 @@
-/*----------------------------------------------------------------------*
- * extEEPROM.h - Arduino library to support external I2C EEPROMs.       *
- * Devices supported include:                                           *
- * - 24XX256 series 256k bit/32k Byte                                   *
- * - M24M02 2M bit/256k Byte                                            *
- *                                                                      *
- * To use the extEEPROM library, the Arduino Wire library must be       *
- * included.                                                            *
- *                                                                      *
- * Jack Christensen 23Mar2013 v1                                        *
- * 29Mar2013 v2 - updated to span page boundaries (and therefore also   *
- * device boundaries, assuming an integral number of pages per device)  *
- *                                                                      *
- * This work is licensed under the Creative Commons Attribution-        *
- * ShareAlike 3.0 Unported License. To view a copy of this license,     *
- * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
- * letter to Creative Commons, 171 Second Street, Suite 300,            *
- * San Francisco, California, 94105, USA.                               *
- *----------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*
+ * extEEPROM.cpp - Arduino library to support external I2C EEPROMs.            *
+ *                                                                             *
+ * This library will work with most I2C serial EEPROM chips between 2k bits    *
+ * and 2048k bits (2M bits) in size. Multiple EEPROMs on the bus are supported *
+ * as a single address space. Certain assumptions are made regarding the       *
+ * EEPROM device addressing. These assumptions should be true for most EEPROMs *
+ * but there are exceptions, so read the datasheet and know your hardware.     *
+ *                                                                             *
+ * The library should also work for EEPROMs smaller than 2k bits, assuming     *
+ * that there is only one on the bus and also that the user is careful not     *
+ * to exceed the maximum address for the EEPROM.                               *
+ *                                                                             *
+ * Device addressing assumptions:                                              *
+ * 1. The I2C address sequence consists of a control byte followed by one      *
+ *    address byte (for EEPROMs <= 16k bits) or two address bytes (for         *
+ *    EEPROMs > 16k bits).                                                     *
+ * 2. The three least-significant bits in the control byte (excluding the R/W  *
+ *    bit) comprise the three most-significant bits for the entire address     *
+ *    space, i.e. all chips on the bus. As such, these may be chip-select      *
+ *    bits or block-select bits (for individual chips that have an internal    *
+ *    block organization), or a combination of both (in which case the         *
+ *    block-select bits must be of lesser significance than the chip-select    *
+ *    bits).                                                                   *
+ * 3. Regardless of the number of bits needed to address the entire address    *
+ *    space, the three most-significant bits always go in the control byte.    *
+ *    Depending on EEPROM device size, this may result in one or more of the   *
+ *    most significant bits in the I2C address bytes being unused (or "don't   *
+ *    care").                                                                  *
+ *                                                                             *
+ * To use the extEEPROM library, the Arduino Wire library must also            *
+ * be included.                                                                *
+ *                                                                             *
+ * Jack Christensen 23Mar2013 v1                                               *
+ * 29Mar2013 v2 - Updated to span page boundaries (and therefore also          *
+ * device boundaries, assuming an integral number of pages per device)         *
+ * 08Jul2014 v3 - Generalized for 2kb - 2Mb EEPROMs.                           *
+ *                                                                             *
+ * External EEPROM Library by Jack Christensen is licensed under CC BY-SA 4.0, *
+ * http://creativecommons.org/licenses/by-sa/4.0/                              *
+ *-----------------------------------------------------------------------------*/
 
 #include <extEEPROM.h>
 #include <Wire.h>
 
-/*----------------------------------------------------------------------*
- * Constructor.                                                         *
- * deviceCapacity is the capacity of a single EEPROM device in          *
- * kilobytes (kB) and should be either 32 or 256. Other values will     *
- * result in undefined behavior!                                        *
- * nDevice is the number of EEPROM devices on the I2C bus.              *
- * pageSize is in bytes.                                                *
- *----------------------------------------------------------------------*/
-extEEPROM::extEEPROM(unsigned int deviceCapacity, byte nDevice, unsigned int pageSize)
+// Constructor.
+// - deviceCapacity is the capacity of a single EEPROM device in
+//   kilobits (kb) and should be one of the values defined in the
+//   eeprom_size_t enumeration in the extEEPROM.h file. (Most
+//   EEPROM manufacturers use kbits in their part numbers.)
+// - nDevice is the number of EEPROM devices on the I2C bus (all must
+//   be identical).
+// - pageSize is the EEPROM's page size in bytes.
+extEEPROM::extEEPROM(eeprom_size_t deviceCapacity, byte nDevice, unsigned int pageSize)
 {
     _dvcCapacity = deviceCapacity;
     _nDevice = nDevice;
     _pageSize = pageSize;
-    _totalCapacity = _dvcCapacity * _nDevice * 1024UL;
-    if (_dvcCapacity == 256)
-        _addrShift = 16;
-    else
-        _addrShift = 15;
-    Wire.begin();
+    _totalCapacity = _nDevice * _dvcCapacity * 1024UL / 8;
+    _nAddrBytes = deviceCapacity > kbits_16 ? 2 : 1;       //two address bytes needed for eeproms > 16kbits
+
+    //determine the bitshift needed to isolate the chip select bits from the address to put into the control byte
+    uint16_t kb = _dvcCapacity;
+    if ( kb <= kbits_16 ) _csShift = 8;
+    else if ( kb >= kbits_512 ) _csShift = 16;
+    else {
+        kb >>= 6;
+        _csShift = 12;
+        while ( kb >= 1 ) {
+            ++_csShift;
+            kb >>= 1;
+        }
+    }
 }
 
-/*----------------------------------------------------------------------*
- * Write bytes to external EEPROM.                                      *
- * If the I/O would extend past the top of the EEPROM address space,    *
- * a status of EEPROM_ADDR_ERR is returned. For I2C errors, the status  *
- * from the Arduino Wire library is passed back through to the caller.  *
- *----------------------------------------------------------------------*/
+//initialize the I2C bus and try a dummy write (no data sent)
+//to the device so that the caller can determine whether it is responding.
+byte extEEPROM::begin(uint8_t eepromAddr)
+{
+    _eepromAddr = eepromAddr;
+    Wire.begin(_eepromAddr);
+    Wire.beginTransmission(_eepromAddr);
+    if (_nAddrBytes == 2) i2cWrite(0);      //high addr byte
+    i2cWrite(0);                            //low addr byte
+    return Wire.endTransmission();
+}
+
+// Write bytes to external EEPROM.
+// If the I/O would extend past the top of the EEPROM address space,
+// a status of EEPROM_ADDR_ERR is returned. For I2C errors, the status
+// from the Arduino Wire library is passed back through to the caller.
 byte extEEPROM::write(unsigned long addr, byte *values, byte nBytes)
 {
-    uint8_t deviceAddr;     //I2C device address
-    uint8_t txStatus;       //transmit status
-    uint8_t nWrite;         //number of bytes to write
+    uint8_t ctrlByte;       //control byte (I2C device address & chip/block select bits)
+    uint8_t txStatus = 0;   //transmit status
+    uint16_t nWrite;        //number of bytes to write
     uint16_t nPage;         //number of bytes remaining on current page, starting at addr
-    
+
     if (addr + nBytes > _totalCapacity) {   //will this write go past the top of the EEPROM?
         return EEPROM_ADDR_ERR;             //yes, tell the caller
     }
-    
+
     while (nBytes > 0) {
         nPage = _pageSize - ( addr & (_pageSize - 1) );
         //find min(nBytes, nPage, BUFFER_LENGTH) -- BUFFER_LENGTH is defined in the Wire library.
         nWrite = nBytes < nPage ? nBytes : nPage;
-        nWrite = BUFFER_LENGTH - 2 < nWrite ? BUFFER_LENGTH - 2 : nWrite;   //allow 2 bytes for address
-        deviceAddr = EEPROM_ADDR | (byte) (addr >> _addrShift);
-        Wire.beginTransmission(deviceAddr);
-        i2cWrite( (byte) (addr >> 8) );            //address high byte
-        i2cWrite( (byte) addr );                   //address low byte
+        nWrite = BUFFER_LENGTH - _nAddrBytes < nWrite ? BUFFER_LENGTH - _nAddrBytes : nWrite;
+        ctrlByte = _eepromAddr | (byte) (addr >> _csShift);
+        Wire.beginTransmission(ctrlByte);
+        if (_nAddrBytes == 2) i2cWrite( (byte) (addr >> 8) );   //high addr byte
+        i2cWrite( (byte) addr );                                //low addr byte
         i2cWrite(values, nWrite);
         txStatus = Wire.endTransmission();
         if (txStatus != 0) return txStatus;
-    
+
         //wait up to 50ms for the write to complete
         for (uint8_t i=100; i; --i) {
             delayMicroseconds(500);                //no point in waiting too fast
-            Wire.beginTransmission(deviceAddr);
-            i2cWrite(0); i2cWrite(0);              //high and low address bytes
+            Wire.beginTransmission(ctrlByte);
+            if (_nAddrBytes == 2) i2cWrite(0);      //high addr byte
+            i2cWrite(0);                            //low addr byte
             txStatus = Wire.endTransmission();
             if (txStatus == 0) break;
         }
         if (txStatus != 0) return txStatus;
-        
+
         addr += nWrite;         //increment the EEPROM address
         values += nWrite;       //increment the input data pointer
         nBytes -= nWrite;       //decrement the number of bytes left to write
@@ -89,17 +131,15 @@ byte extEEPROM::write(unsigned long addr, byte *values, byte nBytes)
     return txStatus;
 }
 
-/*----------------------------------------------------------------------*
- * Read bytes from external EEPROM.                                     *
- * If the I/O would extend past the top of the EEPROM address space,    *
- * a status of EEPROM_ADDR_ERR is returned. For I2C errors, the status  *
- * from the Arduino Wire library is passed back through to the caller.  *
- *----------------------------------------------------------------------*/
+//Read bytes from external EEPROM.
+//If the I/O would extend past the top of the EEPROM address space,
+//a status of EEPROM_ADDR_ERR is returned. For I2C errors, the status
+//from the Arduino Wire library is passed back through to the caller.
 byte extEEPROM::read(unsigned long addr, byte *values, byte nBytes)
 {
-    byte deviceAddr;
+    byte ctrlByte;
     byte rxStatus;
-    uint8_t nRead;              //number of bytes to read
+    uint16_t nRead;             //number of bytes to read
     uint16_t nPage;             //number of bytes remaining on current page, starting at addr
 
     if (addr + nBytes > _totalCapacity) {   //will this read take us past the top of the EEPROM?
@@ -110,14 +150,14 @@ byte extEEPROM::read(unsigned long addr, byte *values, byte nBytes)
         nPage = _pageSize - ( addr & (_pageSize - 1) );
         nRead = nBytes < nPage ? nBytes : nPage;
         nRead = BUFFER_LENGTH < nRead ? BUFFER_LENGTH : nRead;
-        deviceAddr = EEPROM_ADDR | (byte) (addr >> _addrShift);
-        Wire.beginTransmission(deviceAddr);
-        i2cWrite( (byte) (addr >> 8) );            //address high byte
-        i2cWrite( (byte) addr );                   //address low byte
+        ctrlByte = _eepromAddr | (byte) (addr >> _csShift);
+        Wire.beginTransmission(ctrlByte);
+        if (_nAddrBytes == 2) i2cWrite( (byte) (addr >> 8) );   //high addr byte
+        i2cWrite( (byte) addr );                                //low addr byte
         rxStatus = Wire.endTransmission();
         if (rxStatus != 0) return rxStatus;        //read error
-    
-        Wire.requestFrom(deviceAddr, nRead);
+
+        Wire.requestFrom(ctrlByte, nRead);
         for (byte i=0; i<nRead; i++) values[i] = i2cRead();
 
         addr += nRead;          //increment the EEPROM address
--- extEEPROM.h
+++ extEEPROM.h
@@ -1,30 +1,52 @@
-/*----------------------------------------------------------------------*
- * extEEPROM.h - Arduino library to support external I2C EEPROMs.       *
- * Devices supported include:                                           *
- * - 24XX256 series 256k bit/32k Byte                                   *
- * - M24M02 2M bit/256k Byte                                            *
- *                                                                      *
- * To use the extEEPROM library, the Arduino Wire library must be       *
- * included.                                                            *
- *                                                                      *
- * Jack Christensen 23Mar2013 v1                                        *
- * 29Mar2013 v2 - updated to span page boundaries (and therefore also   *
- * device boundaries, assuming an integral number of pages per device)  *
- *                                                                      *
- * This work is licensed under the Creative Commons Attribution-        *
- * ShareAlike 3.0 Unported License. To view a copy of this license,     *
- * visit http://creativecommons.org/licenses/by-sa/3.0/ or send a       *
- * letter to Creative Commons, 171 Second Street, Suite 300,            *
- * San Francisco, California, 94105, USA.                               *
- *----------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*
+ * extEEPROM.cpp - Arduino library to support external I2C EEPROMs.            *
+ *                                                                             *
+ * This library will work with most I2C serial EEPROM chips between 2k bits    *
+ * and 2048k bits (2M bits) in size. Multiple EEPROMs on the bus are supported *
+ * as a single address space. Certain assumptions are made regarding the       *
+ * EEPROM device addressing. These assumptions should be true for most EEPROMs *
+ * but there are exceptions, so read the datasheet and know your hardware.     *
+ *                                                                             *
+ * The library should also work for EEPROMs smaller than 2k bits, assuming     *
+ * that there is only one on the bus and also that the user is careful not     *
+ * to exceed the maximum address for the EEPROM.                               *
+ *                                                                             *
+ * Device addressing assumptions:                                              *
+ * 1. The I2C address sequence consists of a control byte followed by one      *
+ *    address byte (for EEPROMs <= 16k bits) or two address bytes (for         *
+ *    EEPROMs > 16k bits).                                                     *
+ * 2. The three least-significant bits in the control byte (excluding the R/W  *
+ *    bit) comprise the three most-significant bits for the entire address     *
+ *    space, i.e. all chips on the bus. As such, these may be chip-select      *
+ *    bits or block-select bits (for individual chips that have an internal    *
+ *    block organization), or a combination of both (in which case the         *
+ *    block-select bits must be of lesser significance than the chip-select    *
+ *    bits).                                                                   *
+ * 3. Regardless of the number of bits needed to address the entire address    *
+ *    space, the three most-significant bits always go in the control byte.    *
+ *    Depending on EEPROM device size, this may result in one or more of the   *
+ *    most significant bits in the I2C address bytes being unused (or "don't   *
+ *    care").                                                                  *
+ *                                                                             *
+ * To use the extEEPROM library, the Arduino Wire library must also            *
+ * be included.                                                                *
+ *                                                                             *
+ * Jack Christensen 23Mar2013 v1                                               *
+ * 29Mar2013 v2 - Updated to span page boundaries (and therefore also          *
+ * device boundaries, assuming an integral number of pages per device)         *
+ * 08Jul2014 v3 - Generalized for 2kb - 2Mb EEPROMs.                           *
+ *                                                                             *
+ * External EEPROM Library by Jack Christensen is licensed under CC BY-SA 4.0, *
+ * http://creativecommons.org/licenses/by-sa/4.0/                              *
+ *-----------------------------------------------------------------------------*/
 
 #ifndef extEEPROM_h
 #define extEEPROM_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-#include <Arduino.h> 
+#include <Arduino.h>
 #else
-#include <WProgram.h> 
+#include <WProgram.h>
 #endif
 
 //define release-independent I2C functions
@@ -36,24 +58,39 @@
 #define i2cWrite Wire.send
 #endif
 
-//24XX256 I2C Address
-#define EEPROM_ADDR 0x50
+//EEPROM size in kilobits. EEPROM part numbers are usually designated in k-bits.
+enum eeprom_size_t {
+    kbits_2 = 2,
+    kbits_4 = 4,
+    kbits_8 = 8,
+    kbits_16 = 16,
+    kbits_32 = 32,
+    kbits_64 = 64,
+    kbits_128 = 128,
+    kbits_256 = 256,
+    kbits_512 = 512,
+    kbits_1024 = 1024,
+    kbits_2048 = 2048
+};
 
 //EEPROM addressing error, returned by write() or read() if upper address bound is exceeded
-#define EEPROM_ADDR_ERR 9
+const uint8_t EEPROM_ADDR_ERR = 9;
 
 class extEEPROM
 {
     public:
-        extEEPROM(unsigned int deviceCapacity, byte nDevice, unsigned int pageSize);
+        extEEPROM(eeprom_size_t deviceCapacity, byte nDevice, unsigned int pageSize);
+        byte begin(byte eepromAddr = 0x50);
         byte write(unsigned long addr, byte *values, byte nBytes);
         byte read(unsigned long addr, byte *values, byte nBytes);
-        
+
     private:
-        uint16_t _dvcCapacity;          //capacity of one EEPROM device, in kilobytes (kB)
+        uint8_t _eepromAddr;            //eeprom i2c address
+        uint16_t _dvcCapacity;          //capacity of one EEPROM device, in kbits
         uint8_t _nDevice;               //number of devices on the bus
-        uint16_t _pageSize;
-        uint8_t _addrShift;
+        uint16_t _pageSize;             //page size in bytes
+        uint8_t _csShift;               //number of bits to shift address for chip select bits in control byte
+        uint16_t _nAddrBytes;           //number of address bytes (1 or 2)
         unsigned long _totalCapacity;   //capacity of all EEPROM devices on the bus, in bytes
 };
 
